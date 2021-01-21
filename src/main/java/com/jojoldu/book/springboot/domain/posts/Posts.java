f014ff21923769b806f23b608176490abb39eaf3
한지현 <gksgpals96@naver.com>
2021-01-22 00:19:02 +0900

    feat:JPA CRUD Practice

--- src/main/java/com/jojoldu/book/springboot/domain/posts/Posts.java
+++ src/main/java/com/jojoldu/book/springboot/domain/posts/Posts.java
@@ -10,7 +10,7 @@ import javax.persistence.*;
 @NoArgsConstructor //lombok annotation , 기본 생성자 자동 추가 Public Posts(){}와 같은 효과
 @Entity //JPA annotation
 // @Entity는 기본값으로 클래스의 카멜케이스 이름을 언더스코어 네이밍(_)으로 테이블 이름을 매칭 SalesManager.java -> sales_manager table
-public class Posts { //Posts class - DB 테이블과 매칭될 클래스이다. : Entity 클래스라 한다
+public class Posts extends BaseTimeEntity { //Posts class - DB 테이블과 매칭될 클래스이다. : Entity 클래스라 한다
     //DB 데이터 작업을 할 때 실제 쿼리를 날리지 않고, Entity 클래스를 수정한다.
 
     //@GeneratedValue : PK의 생성규칙 나타냄
@@ -37,6 +37,10 @@ public class Posts { //Posts class - DB 테이블과 매칭될 클래스이다. 
         this.title = title;
         this.content = content;
         this.author = author;
+    }
 
+    public void update(String title, String content) {
+        this.title = title;
+        this.content = content;
     }
 }
