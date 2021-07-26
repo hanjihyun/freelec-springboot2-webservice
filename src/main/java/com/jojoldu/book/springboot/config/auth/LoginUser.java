f5a899e705fdf3f6c2b03cdd29360a3a7ff90011
한지현 <gksgpals96@naver.com>
2021-07-26 15:16:45 +0900

    Add : annotation solution

--- src/main/java/com/jojoldu/book/springboot/config/auth/LoginUser.java
+++ src/main/java/com/jojoldu/book/springboot/config/auth/LoginUser.java
@@ -5,7 +5,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target(ElementType.PARAMETER)
-@Retention(RetentionPolicy.RUNTIME)
-public @interface LoginUser {
+@Target(ElementType.PARAMETER) //PARAMETER 로 선언된 객체에서만 사용
+@Retention(RetentionPolicy.RUNTIME) 
+public @interface LoginUser { //@interface : 이 파일을 어노테이션 클래스로 지정, LoginUSer라는 이름을 가진 어노테이션 생성
 }
